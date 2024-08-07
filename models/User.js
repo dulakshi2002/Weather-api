5315449cd253f5f84a877052593f43f3c1ffb425
Dulakshi Savindhya <dulakshisavindya2002@gmail.com>
2024-08-07 10:12:37 +0530

    Add Comments
    2024/8/6

--- models/User.js
+++ models/User.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+//define the schema
 const UserSchema = new Schema({
     Name : {
         type: String,
-        required: true
+        required: true //this field is mandatory
     },
     Email : {
         type: String,
@@ -24,12 +25,14 @@ const UserSchema = new Schema({
         required: true
     },
     WeatherData : {
-        type : Object,
+        type : Object, //can store various weather-related details
         required: true
     }
 
 })
 
+//create a model
 const User = mongoose.model("User", UserSchema);
 
+//export user model for use in other parts
 module.exports= User;
--- server.js
+++ server.js
@@ -2,26 +2,31 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const dotenv = require("dotenv");
-const app = express();
-require("dotenv").config();
+const dotenv = require("dotenv");//module to load environment variables from a .env file
 
+const app = express(); //initialize express application
+require("dotenv").config(); 
 
+//port configuration
 const PORT = process.env.PORT || 8070;
 
 app.use(cors());
 app.use(bodyParser.json());
 
+//mongoDB connection URL
 const URL = process.env.MONGODB_URL;
 
+//connect to monogoDB using mongoose
 mongoose.connect(URL, 
 )
 
+//Connection Success process
 const connection = mongoose.connection;
 connection.once("open", ()=> {
     console.log("Mongodb Connection Success!");
 })
 
+//import routes
 const userRouter = require("./routes/users.js");
 
 http://localhost:8070/user
@@ -32,6 +37,7 @@ app.get("/weather", (req, res) =>{
 
 app.use("/user", userRouter);
 
+//start the server
 app.listen(PORT, () => {
     console.log(`Server is up and running on port no: ${PORT}`)
 })
